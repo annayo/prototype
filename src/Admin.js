2787e572ef0215f6b866b0681608033a47471dd5
anna yovandich <anna.yovandich@ff0000.com>
2017-07-26 18:11:46 -0700

    Added routes and component stubs for Home and Admin

---
+++ src/Admin.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Admin(props) {
+  return (
+    <div className="Admin">
+      <h2>App Admin</h2>
+    </div>
+  );
+}
+
+export default Admin;
--- src/App.js
+++ src/App.js
@@ -1,19 +1,30 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import {
+  BrowserRouter as Router,
+  Route,
+  Link,
+  Redirect
+} from 'react-router-dom'
+import Home from './Home';
+import Admin from './Admin';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+      <Router>
+        <div>
+          <ul>
+            <li><Link to="/">Home</Link></li>
+            <li><Link to="/admin">Admin</Link></li>
+          </ul>
+
+          <hr/>
+
+          <Route exact path="/" component={Home}/>
+          <Route exact path="/admin" component={Admin}/>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      </Router>
     );
   }
 }
