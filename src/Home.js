a994a2fc99c5acc0b124e2e1c35acbd086fd6c80
anna yovandich <anna.yovandich@ff0000.com>
2017-07-27 13:41:50 -0700

    Added Route component to Home, switched to es6 function

--- src/Home.js
+++ src/Home.js
@@ -1,9 +1,23 @@
 import React from 'react';
+import {
+  Route,
+  Link
+} from 'react-router-dom';
+import Device from './Device';
 
-function Home(props) {
+const Home = (props) => {
   return (
     <div className="Home">
       <h2>App Home</h2>
+      <ul>
+        <li><Link to="/appletv">Device Name</Link></li>
+      </ul>
+
+      <hr/>
+
+      <Route path={"/:name"} render={(routeProps) => (
+        <Device {...props} {...routeProps} />
+      )}/>
     </div>
   );
 }
