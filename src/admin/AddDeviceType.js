63b9216dfd684a4cc43b34f50a54b7b16d4828c7
anna yovandich <anna.yovandich@ff0000.com>
2017-07-27 18:30:29 -0700

    Changed to class component for deviceType controls handling

--- src/admin/AddDeviceType.js
+++ src/admin/AddDeviceType.js
@@ -1,41 +1,57 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { v4 } from 'node-uuid';
+import AddControlToDeviceType from './AddControlToDeviceType';
 
-function AddDeviceType(props) {
-  // TODO: provide link to admin/controls if !controls.length
+class AddDeviceType extends Component {
+  constructor(props) {
+    super(props);
 
-  const {
-    id,
-    type,
-    endpoint,
-    control,
-    controls,
-    onAdd,
-    onCancel
-  } = props;
+    this.state = {
+    }
+  }
 
-  const submitLabel = id ? 'Update' : 'Add';
+  render() {
+    // TODO: provide link to admin/controls if !controls.length
 
-  return (
-    <form onSubmit={onAdd}>
-      <select name="type" defaultValue={type}>
-        <option value="apple-tv">Apple TV</option>
-        <option value="samgsung-audio">Samsung Audio</option>
-      </select>
-      <input type="text" placeholder="HTTP Endpoint" name="endpoint" defaultValue={endpoint} />
-      <select name="control" defaultValue={control}>
-        {
-          controls.map((item, i) => {
-            return <option key={i} value={item.id}>{item.name}</option>
-          })
+    const {
+      id,
+      type,
+      endpoint,
+      control,
+      controls,
+      onAdd,
+      onCancel
+    } = this.props;
+
+    const submitLabel = id ? 'Update' : 'Add';
+
+    return (
+      <form onSubmit={onAdd}>
+        <select name="type" defaultValue={type}>
+          <option value="apple-tv">Apple TV</option>
+          <option value="samgsung-audio">Samsung Audio</option>
+        </select>
+        <input type="text" placeholder="HTTP Endpoint" name="endpoint" defaultValue={endpoint} />
+
+        <AddControlToDeviceType controls={controls} />
+
+        <select name="control" defaultValue={control}>
+          <option value={null}>Select a control</option>
+          {
+            controls.map((item, i) => {
+              return <option key={i} value={item.id}>{item.name}</option>
+            })
+          }
+        </select>
+        { id &&
+          <input type="hidden" name="id" value={id} />
         }
-      </select>
-      { id &&
-        <input type="hidden" name="id" value={id} />
-      }
-      <input type="submit" value={submitLabel} />
-      <span onClick={onCancel}>Close</span>
-    </form>
-  );
+        <input type="hidden" name="controlIDs" value="testID" />
+        <input type="submit" value={submitLabel} />
+        <span onClick={onCancel}>Close</span>
+      </form>
+    );
+  }
 }
 
 export default AddDeviceType;
